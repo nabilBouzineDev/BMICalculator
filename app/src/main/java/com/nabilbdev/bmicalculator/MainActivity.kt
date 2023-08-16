245e0ba9ae17a76aa56cebe01fc13ba886c51c65
Nabil Bouzine <nabil@nabilBouzineDev>
2023-08-16 12:42:27 +0100

    first commit

---
+++ app/src/main/java/com/nabilbdev/bmicalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nabilbdev.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
